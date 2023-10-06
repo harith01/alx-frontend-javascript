391b2d9f22fb66af752298cf02c6622862dc1b98
harith01 <harithyusuph@gmail.com>
2023-10-06 11:19:00 +0100

    Task 2

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,8 @@
+export default function getStudentsByLocation(students, city) {
+    return (
+        students.filter((student) => {
+            return student.location === city
+        })
+    )
+}
+
