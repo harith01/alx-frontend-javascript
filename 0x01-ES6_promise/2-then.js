84a9aeb449642335a157d375b31680f887683aa8
harith01 <harithyusuph@gmail.com>
2024-04-23 22:25:01 +0100

    Task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   return (
     promise.finally(() => console.log('Got a response from the API'))
-      .then(() => ({ status: 200, body: 'Success' }), new Error())
+      .then(() => ({ status: 200, body: 'success' }), new Error())
   );
 }
