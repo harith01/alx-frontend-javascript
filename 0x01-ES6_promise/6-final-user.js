690025cdbba251979367c6a688a0a994b46df4c4
harith01 <harithyusuph@gmail.com>
2024-04-24 12:17:47 +0100

    Task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,6 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => values);
+  // eslint-disable-next-line
+    .then((values) => {});
 }
