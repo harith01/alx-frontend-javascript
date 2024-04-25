2acad2e92af421ff4f3d5787738f782524745abf
harith01 <harithyusuph@gmail.com>
2024-04-25 21:03:04 +0100

    Task 1

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,9 @@
+import ClassRooms from './0-classroom';
+
+export default function initializeRooms() {
+  return [
+    new ClassRooms(19),
+    new ClassRooms(20),
+    new ClassRooms(34),
+  ];
+}
