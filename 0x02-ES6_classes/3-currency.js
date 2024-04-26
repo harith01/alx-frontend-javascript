bdc62dc64afca8e42fbeda8d05161b1ec2bd5f7c
harith01 <harithyusuph@gmail.com>
2024-04-26 09:26:31 +0100

    Task 3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,34 @@
+/* eslint-disable */
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else {
+      this._name = value;
+    }
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = value;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
