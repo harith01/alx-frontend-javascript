e054c0f97e5ba4ccd7c6651957596aeb86663b97
harith01 <harithyusuph@gmail.com>
2024-04-30 19:31:44 +0100

    Task 10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,9 +2,7 @@ export default function updateUniqueItems(mp) {
   if (!(mp instanceof Map)) {
     throw new Error('Cannot process');
   }
-  for (const [key, value] of mp) {
-    if (value === 1) {
-      mp.set(key, 100);
-    }
-  }
+  mp.forEach((key, value) => {
+    if (value === 1) mp.set(key, 100);
+  });
 }
