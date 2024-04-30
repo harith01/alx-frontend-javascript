e2efcf76e6282f706c6980a5c9b5deefea198c99
harith01 <harithyusuph@gmail.com>
2024-04-30 11:38:27 +0100

    Task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
+  if (startString.length === 0 || typeof set !== 'object' || typeof startString !== 'string') return '';
   const newSet = new Set();
-  if (startString.length === 0) return '';
   set.forEach((el) => {
     if (el.startsWith(startString)) newSet.add(el.slice(startString.length));
   });
