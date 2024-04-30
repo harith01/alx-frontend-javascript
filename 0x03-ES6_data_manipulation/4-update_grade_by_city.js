20ae305eef9c5fee8b4b9f2e713ad5a4c0dd2a28
harith01 <harithyusuph@gmail.com>
2024-04-30 09:57:30 +0100

    Task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,13 +1,25 @@
-export default function updateStudentGradeByCity(students, city, newGrades) {
-  const studentsByCity = students.filter((student) => student.location === city);
-  const studentsByCityUpgrade = studentsByCity.map((student) => {
-    const gradeObject = newGrades.find((grade) => student.id === grade.grade);
-    const updatedGrade = gradeObject ? gradeObject.grade : 'N/A';
-    return {
-      ...student,
-      grade: updatedGrade,
-    };
-  });
+// export default function updateStudentGradeByCity(students, city, newGrades) {
+//   const studentsByCity = students.filter((student) => student.location === city);
+//   const studentsByCityUpgrade = studentsByCity.map((student) => {
+//     const gradeObject = newGrades.find((grade) => student.id === grade.grade);
+//     const updatedGrade = gradeObject ? gradeObject.grade : 'N/A';
+//     return {
+//       ...student,
+//       grade: updatedGrade,
+//     };
+//   });
 
-  return studentsByCityUpgrade;
+//   return studentsByCityUpgrade;
+// }
+
+
+export default function updateStudentGradeByCity(students, city, newGrades) {
+    return students.filter((student) => student.location === city)
+                   .map((student) => {
+                     const gradeObject = newGrades.find((grade) => student.id === grade.studentId)
+                     return {
+                        ...student,
+                        grade: gradeObject ? gradeObject.grade: 'N/A'
+                     }
+                   })
 }
