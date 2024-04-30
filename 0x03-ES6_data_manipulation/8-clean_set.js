cf124b11ffdf28f625e04fa01e8449891871b8c0
harith01 <harithyusuph@gmail.com>
2024-04-30 17:52:33 +0100

    Task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,28 @@
+// export default function cleanSet(set, startString) {
+//   if (startString.length === 0 || typeof set !== 'object' || typeof startString !== 'string') return '';
+//   const list = [];
+//   set.forEach((el) => {
+//     if (el.startsWith(startString)) list.push(el.slice(startString.length));
+//   });
+//   return list.join('-');
+// }
+
 export default function cleanSet(set, startString) {
-  if (startString.length === 0 || typeof set !== 'object' || typeof startString !== 'string') return '';
-  const list = [];
-  set.forEach((el) => {
-    if (el.startsWith(startString)) list.push(el.slice(startString.length));
-  });
-  return list.join('-');
-}
+    const list = [];
+  
+    if (
+      typeof set !== 'object'
+      || typeof startString !== 'string'
+      || startString.length === 0
+    ) {
+      return '';
+    }
+  
+    for (const item of set) {
+      if (item && item.startsWith(startString)) {
+        list.push(item.slice(startString.length));
+      }
+    }
+  
+    return list.join('-');
+  }
