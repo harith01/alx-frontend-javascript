20ffb45efd5b1450f668019014a394a5a866a11f
harith01 <harithyusuph@gmail.com>
2024-05-01 13:19:33 +0100

    Task 1

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -8,4 +8,50 @@ interface Teacher {
     [key: string]: any;   //add any attribute to the object
 }
 
+// The Directors interface that extends the Teacher interface
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+// Interface for the function, printTeacherFunction
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+// printTeacher function
+const printTeacher: printTeacherFunction = function(firstName, lastName) {
+    return `${firstName[0]}. ${lastName}`;
+}
+
+// Student class interface
+interface StudentClassInterface {
+   firstName: string;
+   workOnHomework(): string;
+   displayName(): string;
+}
+
+// Student class constructor interface
+interface StudentClassConstructor {
+    new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+//Student class
+const StudentClass = class StudentClass implements StudentClassInterface {
+
+    firstName: string;
+    lastName: string;
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return "Currently working";
+    }
+    displayName(): string {
+        return this.firstName;
+    }
+
+
+}
 
