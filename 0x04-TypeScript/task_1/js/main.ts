17bd9c85dc03a1d11ba3b85dac856a2e80620343
harith01 <harithyusuph@gmail.com>
2024-05-01 11:48:50 +0100

    Task 1

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,11 @@
+// The Teacher interface
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any;   //add any attribute to the object
+}
+
+
